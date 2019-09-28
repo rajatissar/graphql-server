f165547101ab0de8a3f8e3ed04c76e5c9e7eb6e8
Rajat Kumar <rajatissar94@gmail.com>
2019-09-28 19:29:24 +0530

    port issue resolved

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import express_HTTP from 'express-graphql';
 import user_schema from './data/schema';
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.use(
